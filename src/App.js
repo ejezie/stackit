4415f554a255e1af7bedf35c15c5584a83be1be6
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-02-19 21:58:54 +0100

    header in progress

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import styled,{createGlobalStyle} from "styled-components";
+import React, { Component } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './components/Header';
+// import Feed from './Feed';
 
 const GlobalStyle = createGlobalStyle`
-body{
-  margin: 0;
-  padding: 0;
-  background-color: red;
-}`;
 
+  *{
+    @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: 'Raleway', sans-serif;
+  }
+`;
 
-function App() {
-  return (
-    <>
-      <GlobalStyle/>
-      <h1>Hello</h1>
-    </>
-  );
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <AppWrapper>
+          <Header />
+          {/* <Feed /> */}
+           <h1>hello marry madlin</h1>
+        </AppWrapper>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from '../assets/coding.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <h1>Stack</h1>
+      <img src={logo} alt="logo" />
+      <h1>Over</h1>
+    </div>
   )
 }
 
