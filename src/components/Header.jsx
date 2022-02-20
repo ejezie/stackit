f5d47d03265ee003a7a21c05ad77c9d694dbc8c2
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-02-20 21:06:53 +0100

    styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import logo from '../assets/coding.png'
+import styled from 'styled-components'
+
+const HeaderWrap = styled.div`
+display : flex;`
 
 function Header() {
   return (
-    <div>
+    <HeaderWrap>
       <h1>Stack</h1>
       <img src={logo} alt="logo" />
       <h1>Over</h1>
-    </div>
+    </HeaderWrap>
   )
 }
 
