ff48b6f9c9e301920d40fd9904c3fa060e6e6cf2
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-02-27 21:54:15 +0100

    added titile to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,23 @@ import styled from 'styled-components'
 
 const CardWrap = styled.div`
   width: 80%
-  height: 50rem;
+  // height: 50rem;
   border-radius: 50px;
   background: #e0e0e0;
   box-shadow:  20px 20px 60px #c5c5c5, -20px -20px 60px #fbfbfb;
+  margin: 0 0 2rem 0;
 `;
 
-function Card() {
+function Card({data}) {
   return (
-    <CardWrap>Card</CardWrap>
+    <CardWrap>
+      <div className="top">
+        <div className="tittle">{data.title}</div>
+        <div className="tags">{data.tags.forEach(tag => {
+          <div className="tag">{tag}</div>
+        })}</div>
+      </div>
+    </CardWrap>
   )
 }
 
