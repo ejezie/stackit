d0354e639ee0df6a9ceedb6161639e9883c8957d
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-03-09 23:16:28 +0100

    executing server side rendering

---
+++ server/server.js
@@ -0,0 +1,7 @@
+import path from 'path';
+import fs from 'fs';
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import App from "../src/App"
