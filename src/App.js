d776f8b61cc50ee27ec437d23ffb488af1b72679
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-03-11 13:24:04 +0100

    SSR configuration

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled, { createGlobalStyle } from 'styled-components';
 import Header from './components/Header';
 import Feed from './containers/Feed';
 import Question from './containers/Question';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 // import "../node_modules/react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const GlobalStyle = createGlobalStyle`
@@ -30,13 +30,11 @@ class App extends Component {
         <GlobalStyle />
         <AppWrapper>
           <Header />
-          <BrowserRouter>
             <Routes>
               <Route path="/" element={<Feed/>}/>
               <Route path="/questions" element={<Feed/>}/>
               <Route path="/questions/:id" element={<Question/>}/>
             </Routes>
-          </BrowserRouter>
         </AppWrapper>
       </>
     );
